eb8ed831249ba77ff0f6b69a8fa49e7bba0ea283
Ayush Jain <aj601604@gmail.com>
2024-08-03 23:21:10 +0530

    minor serializer file update

--- core/serializers.py
+++ core/serializers.py
@@ -4,19 +4,20 @@ import pandas as pd
 class CSVUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
-    def validate_file(self, value):
-        if not value.name.endswith('.csv'):
+    def validate_file(self, file):
+        if not file.name.endswith('.csv'):
             raise serializers.ValidationError('Invalid file format. Please upload a CSV file.')
         
         # Check CSV columns
         try:
-            df = pd.read_csv(value)
+            df = pd.read_csv(file)
             
             # Validate required columns
             required_columns = {'S. No.', 'Product Name', 'Input Image Urls'}
             if not required_columns.issubset(df.columns):
                 raise serializers.ValidationError('CSV file is missing required columns: {}'.format(', '.join(required_columns - set(df.columns))))
+
+            return file
+    
         except Exception as e:
             raise serializers.ValidationError(f'Error reading CSV file: {str(e)}')
-        
-        return value
